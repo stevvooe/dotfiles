67d0f132fd0fe56dda691c94938ac21a5c03321a
Stephen Day <stephen.day@docker.com>
2024-02-01 08:34:55 -0800

    nvim: display diagnostics in telescope
    Signed-off-by: Stephen Day <stephen.day@docker.com>

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -11,13 +11,14 @@ map("n", "<leader>fg", "<cmd> Telescope live_grep <CR>")
 map("n", "<leader>fb", "<cmd> Telescope buffers <CR>")
 map("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>")
 map("n", "<leader>fh", "<cmd> Telescope help_tags <CR>")
+map("n", "<leader>d", "<cmd> Telescope diagnostics <CR>")
+map("n", "<leader>D", "<cmd> Telescope diagnostics bufnr=0 <CR>")
 map("n", "<leader>gt", "<cmd> Telescope git_status <CR>")
 
 -- Diagnostics mappings for lsp.
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
--- map("n", "<leader>q", "<cmd> lua vim.diagnostic.setloclist() <CR> <cmd> Telescope loclist <CR>") TODO: integrate telescope.
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 vim.keymap.set("n", "<leader>Q", vim.diagnostic.setqflist)
 
