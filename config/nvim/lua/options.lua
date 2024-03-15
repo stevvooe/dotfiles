73dd48d61276af13a5a5163004463de17cd427f1
Stephen Day <stephen.day@docker.com>
2024-03-14 18:34:52 -0700

    nvim: move back to 4 space tabstops
    Signed-off-by: Stephen Day <stephen.day@docker.com>

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -3,9 +3,9 @@ vim.opt.relativenumber = true
 
 vim.opt.mouse = "a"
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
