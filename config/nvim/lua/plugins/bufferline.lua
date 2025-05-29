cf445838218b6e3205b9fae6bbedde616e6cbc76
Stephen Day <stephen@magic.dev>
2025-05-29 12:33:58 -0700

    nvim: update plugins and resolve old errors
    Signed-off-by: Stephen Day <stephen@magic.dev>

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -2,7 +2,6 @@ return {
   'akinsho/bufferline.nvim',
   version = "*",
   branch = "main",
-  commit = "73540cb95f8d95aa1af3ed57713c6720c78af915",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require("bufferline").setup({
