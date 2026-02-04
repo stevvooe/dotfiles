0db6169488d6d0b464ebae016725efff54a42d5d
Stephen Day <stephen.day@baseten.co>
2026-02-03 17:25:40 -0800

    nvim: move to catpuccin for colorscheme
    Signed-off-by: Stephen Day <stephen.day@baseten.co>

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-  "sainnhe/gruvbox-material",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.gruvbox_material_background = "soft"
-    vim.cmd([[colorscheme gruvbox-material]])
+    require("catppuccin").setup({
+      flavour = "mocha",
+    })
+    vim.cmd([[colorscheme catppuccin]])
   end,
 }
