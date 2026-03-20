68391f86aff5bb21de9000948cd16d2ff8923a18
Stephen Day <stephen.day@baseten.co>
2026-03-20 14:13:46 -0700

    nvim: cleanup configuration
    Signed-off-by: Stephen Day <stephen.day@baseten.co>

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -23,6 +23,5 @@ return {
   { import = "plugins.sleuth" },
   { import = "plugins.undotree" },
   { import = "plugins.fidget" },
-  { import = "plugins.claude-code" },
   { import = "plugins.markdown-preview" },
 }
